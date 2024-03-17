3158ae45c6b80ccb905692c1bcb587c25decf34d
chisu15 <trunghieudo1509@gmail.com>
2024-03-17 08:29:10 +0700

    Add multer, fs, path

--- models/doodle-collection.model.js
+++ models/doodle-collection.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const doodleCollectionSchema = mongoose.Schema({
     title: String,
     description: String,
+    views: Number,
     doodles: [{
         doodle_id: mongoose.Schema.Types.ObjectId
     }],    
